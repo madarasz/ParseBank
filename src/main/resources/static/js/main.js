aeb371790ac28a679f2a3c9cd4620b0a63157970
Istvan Madarasz <mr.madarasz.istvan@gmail.com>
2015-12-10 11:02:10 +0100

    currency formatting

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,1 +1,10 @@
+function formatMoney(input) {
+    input = input.toString();
+    var length = input.length;
+    while (length > 3) {
+        length -= 3;
+        input = input.substring(0, length) + " " + input.substring(length);
+    }
+    return input + " Ft";
+}
 
